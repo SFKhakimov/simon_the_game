3c0180d3d6e8dc704e59522d6b05051bebc55744
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-08-11 22:34:03 +0300

    Добавил радиокнопки для выобра сложности игры

--- src/components/OptionsGame.vue
+++ src/components/OptionsGame.vue
@@ -1,9 +1,70 @@
 <template>
-  <h2>Опции</h2>
+  <form class="speed">
+    <h2 class="speed__title">Уровень сложности:</h2>
+    <label v-for="elem in Object.values(speedSettings)" :key="elem.name">
+      <input
+        v-if="elem.value === speed"
+        type="radio"
+        class="speed__input"
+        :value="elem.value"
+        :checked="true"
+        @change="$emit('change', elem.value)"
+      />
+      <input
+        v-else
+        type="radio"
+        class="speed__input"
+        :value="elem.value"
+        @change="$emit('change', elem.value)"
+      />
+      {{ elem.name }}
+    </label>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    speed: Number,
+    change: Function,
+  },
+  data() {
+    return {
+      speedSettings: {
+        low: {
+          name: "Легкий",
+          value: 1500,
+        },
+        middle: {
+          name: "Средний",
+          value: 1000,
+        },
+        hight: {
+          name: "Сложный",
+          value: 400,
+        },
+      },
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.speed {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 200px;
+  margin: 0 auto;
+
+  &__title {
+    margin: 0 auto;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+
+  &__input {
+    margin-left: 50px;
+  }
+}
+</style>
